51dab5693ea9bd64a566b7bb6c3e23eb3fed162b
SamanthaJowa <94848504+SamanthaJowa@users.noreply.github.com>
2022-04-11 09:41:22 +0200

    Assignment codes

---
+++ ML-Assignment/app.py
@@ -0,0 +1,8 @@
+def example():
+    for i in range(6):
+        meal = 'sadza'
+
+    dinner = 'beef '+ meal
+
+    print(dinner)
+example()
---
+++ ML-Assignment/pages/about.py
@@ -0,0 +1,20 @@
+#%%writefile about.py
+import streamlit as st
+
+def app():
+    
+    st.title('Object Detection using InceptionV3 Model...')
+
+    c1, c2, c3 = st.columns([4,2,4])
+    with c2:
+        st.write('Done by:')
+
+    b1,m1,l1 = st.columns([3,1,3])
+    with b1:
+        st.write('Genius Mpala')
+        st.write('geniustuta@gmail.com')
+    with m1:
+        st.write("|")
+    with l1:
+        st.write('')
+        st.write('')
